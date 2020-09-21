fccfaabfcb45682aa95b98d2e498b0f21121dde8
waseem <waseemmedhatg@gmail.com>
2020-09-21 15:01:04 +0200

    push tryCatch

--- git_doneR.R
+++ git_doneR.R
@@ -1,4 +1,5 @@
 #! /usr/bin/Rscript
+
 library("gert")
 
 args = commandArgs(trailingOnly = TRUE)
@@ -11,11 +12,19 @@ if (length(args) == 0) {
 if (nrow(git_status()) == 0) {
   cat("Nothing to commit... exiting\n")
   q("no")
+} else {
+  git_add(".")
+  git_commit(args[1])
 }
 
-git_add(".")
-git_commit(args[1])
-git_pull()
-cat("Successfully pulled!\n")
-git_push()
-cat("Successfully pushed!\n")
+tryCatch(
+  {
+    git_pull()
+    cat("Successfully pulled!\n")
+    git_push()
+    cat("Successfully pushed!\n")
+  },
+  error = function() {
+    cat("Problem with pushing... exiting")
+  }
+)

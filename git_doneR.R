b0b1b6070b21e1aacd89f2454224cef877c4d32c
waseem <waseemmedhatg@gmail.com>
2020-09-21 14:30:39 +0200

    commit 1

--- git_doneR.R
+++ git_doneR.R
@@ -4,7 +4,8 @@ library("gert")
 args = commandArgs(trailingOnly = TRUE)
 
 if (length(args) == 0) {
-  print("No message given... exiting")
+  cat("No message given... exiting\n")
+  q("no")
 }
 
 git_add(".")
